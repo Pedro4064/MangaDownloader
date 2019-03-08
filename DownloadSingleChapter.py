2b91ae32f393398dc70e24d80dc9f9cc2f23d5aa
Pedro Cruz <pedrohlcruz@gmail.com>
2019-03-08 20:46:25 -0300

    Update DownloadSingleChapter.py

--- DownloadSingleChapter.py
+++ DownloadSingleChapter.py
@@ -9,7 +9,6 @@ imgLinks = []
 
 
 #Link of the mainpage(with all the other links)
-mainLink = "https://kissmanga.com/Manga/Go-Toubun-no-Hanayome"
 imagexPath = '//*[@id="divImage"]/p[%s]/img'
 imageName = '%s.JPEG'
 counter = 0
@@ -19,11 +18,14 @@ print('              ____                      __                __         \n  
 print("\n")
 
 directory = input("Inser the directory to download all the images: ")
-pdfDirectory = input("Insert the direcoty to move the pdf to: ")
+pdfDirectory = input("Insert the directory to move the pdf to: ")
 volLink = input("Insert the volume's link: ")
-volNumber = input("Insert the volume Number:")
+volNumber = input("Insert the volume Number: ")
+nPages = input('Insert the number of pages in the volume: ')
+
 
 os.system("clear")
+print("\n")
 
 print("Are all the informations correct? ")
 print("Images's direcotry:",directory)
@@ -35,30 +37,33 @@ answer = input("[y/n] ")
 
 if answer == "n":
     quit()
+
 else:
     driver.get(volLink)
     time.sleep(15)
 
-    for i in range(40): #Change to overshoot the number of pages
+    for i in range(int(nPages)+1): #adds one so it overshoots the user input to make sure
         i+=1
         time.sleep(5)
 
-        try:
+        try: #Adds the link of each page to the imgLinks list
             imgLinks.append(driver.find_element_by_xpath(imagexPath%(i)).get_attribute('src'))
-        except:
-            print("Failed to get the link for the",str(i),"page")
+
+        except:#Print the number of the page it encounterred an error
+            print(colored("[ERROR] Failed to get the link for the "+str(i)+" page",'red'))
             continue
 
     print("Gathered all the link, moving to "+directory)
-    os.chdir(directory)
+    os.chdir(directory) #move to the directory specified by the user to save the images
 
-    for link in imgLinks:
+    for link in imgLinks: #goes through each page/image link
 
+
+    #counter < 10 because from 0-9 the name is 00x.JPEG, but from 10 onward it's 0x.JPEG ->where x is the number of the page (to make sure the order is correct when making the pdf)
         if counter < 10:
-
             response = requests.get(link)
 
-            with open("00%s.JPEG"%(counter),'wb') as image:
+            with open("00%s.JPEG"%(counter),'wb') as image:#Creates a .JPEG file and downloads the image from the link
                 image.write(response.content)
 
         if counter >= 10:
@@ -71,4 +76,14 @@ else:
 
         counter+=1
 
-    os.system("convert *.JPEG Vol.%s.pdf"%(volNumber))
+    try:
+        if int(volNumber) < 10:
+            os.chdir(pdfDirectory)
+            os.system("convert *.JPEG Vol.00%s.pdf"%(volNumber))
+
+        else:
+            os.chdir(pdfDirectory)
+            os.system("convert *.JPEG Vol.0%s.pdf"%(volNumber))
+
+    except:
+        print(colored("[ERROR] Could not change to the finale/pdf directory",'red'))

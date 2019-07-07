a2d2a35abf792c92cd1542d60eca47c669ec8a7e
Pedro Cruz <pedrohlcruz@gmail.com>
2019-07-07 17:04:53 -0300

    .

--- dailyCheck.py
+++ dailyCheck.py
@@ -17,15 +17,120 @@ import os
 
 userName = 'pedrohlcruz@gmail.com'
 password = 'waqfpbvuafucqeyu'
+mangaName = ''
+
+name = ''
+mainLink = ''
+lastLink = ''
+
+
+#Specifies the direcotry of the webdriver
+driverPath = '/Applications/chromedriver'
+
+#Add the headless option
+options = Options()
+options.add_argument('--headless')
+
+print(mainLink)
+driver = webdriver.Chrome( options = options, executable_path=driverPath)
+
 
 def readFile():
+
     print("Reading file")
+    
+
+    # open the csv file
+    with open('info.csv','r') as file:
+        info = csv.reader(file,delimiter = ',')
+
+        for line in info:
+    
+            global name
+            global mainLink
+            global lastLink
+
+            name = line[0]
+            mainLink = line[1]
+            lastLink = line[2]
+
+            # Call the check Function
+            checkNewVolume()       
 
 def checkNewVolume():
+
     print("Chekcing new voluems")
 
-def downloadVolume():
+    #Declare strings used to scrape the site
+    xPath  = '//*[@id="leftside"]/div[2]/div[2]/div[2]/table/tbody/tr[3]/td[1]/a'
+    
+    # go to the main link
+    driver.get(mainLink)
+
+    time.sleep(15)
+
+    # save the link found so it can compre it to the one in the csv file
+    latestLink = driver.find_element_by_xpath(xPath).get_attribute('href')
+    
+
+    # check if the newest link is the last downloaded volume, if no, it means it is a new volume
+    if latestLink == lastLink:
+        print('All uptodate')
+        downloadVolume(latestLink)
+
+    else:
+        print('Downloading the new file')
+        downloadVolume(latestLink)
+
+def downloadVolume(link):
+
+    # Change the directory 
+    os.chdir('/Users/pedrocruz/Desktop/MangaDownloader/mangaTest')
+
     print("Downloading new volume")
+    imagexPath = '//*[@id="divImage"]/p[%s]/img'
+
+    driver.get(link)
+    # a list with all the pages' links 
+    imgLinks = []
+
+    # Creates the progrss bar 
+    linkBar = IncrementalBar('Gathering the image links', max = 100)
+
+    # goes and tries to get 100 pages 
+    for i in range(1,101):
+
+        try:
+            imgLinks.append(driver.find_element_by_xpath(imagexPath%(i)).get_attribute('src'))
+        
+        except:
+            pass
+
+        # update the progrss bar 
+        linkBar.next()
+
+    # Downloads the images with the requests library
+    page = 1
+    for link in imgLinks:
+
+        # Creates a file to save the image in
+        fileName = ""
+        if page<10:
+            fileName = "00%d.png"%(page)
+
+        elif page >=10 and page < 100:
+            fileName = "0%d.png"%(page)
+
+        else :
+            fileName = "%d.png"%(page)
+
+        response = requests.get(link)
+        
+        with open(fileName,'wb') as image:
+            image.write(response.content)
+        
+        page+=1
+
 
 def sendMail():
 
@@ -40,29 +145,30 @@ def sendMail():
     # storing the receivers email address  
     msg['To'] = userName 
     
-    # storing the subject  
-    msg['Subject'] = "new Manga volume added"
+    # storing the subject  and format the string to add the manga title 
+    msg['Subject'] = "New volume added for %s" %()
     
     filename = "as-Vol.0092.pdf"
     attachment = open("as-Vol.0092.pdf", "rb") 
     
     # instance of MIMEBase and named as p 
-    p = MIMEBase('application', 'octet-stream') 
+    pdf = MIMEBase('application', 'octet-stream') 
 
     # To change the payload into encoded form 
-    p.set_payload((attachment).read()) 
+    pdf.set_payload((attachment).read()) 
     
     # encode into base64 
-    encoders.encode_base64(p) 
+    encoders.encode_base64(pdf) 
     
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+    pdf.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
     
     # attach the instance 'p' to instance 'msg' 
-    msg.attach(p)
+    msg.attach(pdf)
 
     # Converts the Multipart msg into a string 
     text = msg.as_string()
     
+    # Starts the mail connection 
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
@@ -70,10 +176,11 @@ def sendMail():
 
     server.login(userName,password)
 
-    # message = 'Subject: python Test'
-
+    # Send the message adn quit 
     server.sendmail(userName,userName,text)
     server.quit()
 
 
-sendMail()
+# sendMail()
+readFile()
+
